cb51c355d44fbc42788fc12b486efc460b728a62
Fernando Carrera <fcarrera@asematec.com>
2020-06-08 22:48:11 -0500

    Calcula la distancia de la rampa en centímetros y formateado a 2 decimales instanciando clases

--- Actividad1/src/main/java/com/mycompany/actividad1/Resultados.java
+++ Actividad1/src/main/java/com/mycompany/actividad1/Resultados.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.actividad1;
 
 /**
  *
- * @author user
+ * @author Fcarrera
  */
 public class Resultados {
          public static void main(String[] args) {
